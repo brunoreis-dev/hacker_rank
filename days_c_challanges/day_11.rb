2f43aed4586c913578c7fcce8eb7f21ed6ce4948
brunoreis-dev <b.macedoreis@icloud.com>
2024-01-18 17:44:19 -0300

    to send the c challange of 11th and 12th day, but with ruby lang (because challanges need OO)

---
+++ days_c_challanges/day_11.rb
@@ -0,0 +1,46 @@
+class Person
+	def initialize(firstName, lastName, id)
+		@firstName = firstName
+		@lastName = lastName
+		@id = id
+	end
+	def printPerson()
+		print("Name: ",@lastName , ", " + @firstName ,"\nID: " , @id)
+	end
+end
+
+class Student < Person
+    def initialize(firstName, lastName, id, scores)
+        super(firstName, lastName, id)
+        @scores = scores
+    end
+    
+    def calculate()
+        average = @scores.sum / @scores.length
+
+        case average
+        when 90..100
+            'O'
+        when 80..89
+            'E'
+        when 70..79
+            'A'
+        when 55..69
+            'P'
+        when 40..54
+            'D'
+        else
+            'T'
+        end
+    end
+end
+
+input = gets.split()
+firstName = input[0]
+lastName = input[1]
+id = input[2].to_i
+numScores = gets.to_i
+scores = gets.strip().split().map!(&:to_i)
+s = Student.new(firstName, lastName, id, scores)
+s.printPerson           
+print("\nGrade: " + s.calculate)
