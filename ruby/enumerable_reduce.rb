40c01d2e54496788c7cc69cbe10fd6522cf894b9
brunoreis-dev <b.macedoreis@icloud.com>
2024-03-29 21:41:52 -0300

    adjust reduce sum terms ruby challange

--- ruby/enumerable_reduce.rb
+++ ruby/enumerable_reduce.rb
@@ -1,9 +1,11 @@
 def sum_terms(n)
-  (1..n).reduce(0) { |sum, term| sum + term}
-
-  # Implicit
-  # (1..n).reduce(0, :+)
-
-  # Inject
-  # (1..n).inject(0) {|product, n| product * n }
+  (1..n).reduce(0) {|sum, term| sum + term**2 + 1}
 end
+
+sum_terms(25)
+
+# Implicit
+# (1..n).reduce(0, :+)
+
+# Inject
+# (1..n).inject(0) {|product, n| product * n }
