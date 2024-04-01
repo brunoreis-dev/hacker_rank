e40453e604c725771579b83b054bdfc74a17ed2f
brunoreis-dev <b.macedoreis@icloud.com>
2024-03-31 21:12:56 -0300

    solve methods curry ruby challange

--- ruby/methods_curry.rb
+++ ruby/methods_curry.rb
@@ -2,8 +2,9 @@ power_function = -> (x, z) {
     z ** x
 }
 
-power = 2
-power_of = power_function.curry.(power)
+power_of_two = power_function.curry.(2)
+power_of_three = power_function.curry.(3)
 
 base = 5
-puts power_of.(base)
+puts power_of_two.(base)
+puts power_of_three.(base)
